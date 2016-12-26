b2b36e9050f42b736b10f4fb712d88419069d70c
Zhongwei Zhao <zhongweizhao2016@gmail.com>
2016-12-25 22:00:48 -0500

    new style

---
+++ MATH 146/js/main.js
@@ -0,0 +1,71 @@
+(function(w, d){
+
+    var body = d.body,
+        gotop = d.getElementById('gotop'),
+        menu = d.getElementById("menu");
+        menuToggle = d.getElementById("menuToggle"),
+        mask = d.getElementById("mask"),
+        topheader = d.getElementById("top-header"),
+        animate = w.requestAnimationFrame,
+        ua = navigator.userAgent,
+        isMD = ua.indexOf('Mobile') !== -1 || ua.indexOf('Android') !== -1 || ua.indexOf('iPhone') !== -1 || ua.indexOf('iPad') !== -1 || ua.indexOf('KFAPWI') !== -1,
+        even = isMD ? 'touchstart' : 'click',
+        docEl = ua.indexOf('Firefox') !== -1 ? d.documentElement : body;
+
+    var Content = {
+        goTop: function(){
+            var top = docEl.scrollTop;
+            if (top > 400) {
+                docEl.scrollTop = top - 400;
+                animate(arguments.callee);
+            } else {
+                docEl.scrollTop = 0;
+            }
+        },
+        toggleGotop: function(top) {
+            if (top > w.innerHeight / 2) {
+                gotop.classList.add('in');
+            } else {
+                gotop.classList.remove('in');
+
+            }
+        },
+        toggleMenu: function(flag) {
+            if (flag) {
+                menu.classList.add('show');
+                mask.classList.add('in');
+            }
+        },
+        fixedHeader: function(top) {
+            if (top > topheader.clientHeight) {
+                topheader.classList.add('fixed');
+            } else {
+                topheader.classList.remove('fixed');
+            }
+        },
+    }
+
+    menuToggle.addEventListener(even, function(e) {
+        Content.toggleMenu(true);
+        e.preventDefault();
+    });
+
+    mask.addEventListener(even, function(e){
+        menu.classList.remove('show');
+        mask.classList.remove('in');
+    });
+
+    gotop.addEventListener(even, function() {
+        animate(Content.goTop);
+    }, false);
+
+    d.addEventListener('scroll', function() {
+        var top = docEl.scrollTop;
+        Content.toggleGotop(top);
+        Content.fixedHeader(top);
+    }, false);
+
+
+    Waves.init();
+
+})(window, document);
